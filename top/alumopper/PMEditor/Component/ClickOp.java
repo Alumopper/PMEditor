8515b4d228e55b97b5551f6f5b58c84cbdcd32f2
Alumopper <609255325@qq.com>
2022-10-10 15:56:48 +0800

    w

--- top/alumopper/PMEditor/Component/ClickOp.java
+++ top/alumopper/PMEditor/Component/ClickOp.java
@@ -15,8 +15,8 @@ public class ClickOp {
 	 * 在信息框被点击后，将会执行的某一个操作。重写此方法以用于实现自己的功能。默认点击后使信息框消失
 	 */
 	public void afterClick(){
+		infoBox.setBackground(infoBox.getBackground().brighter());
 		if(!infoBox.isOut()){
-			infoBox.setBackground(infoBox.getBackground().brighter());
 			infoBox.time -= infoBox.time - infoBox.outTime - infoBox.currTime;
 		}
 	}
